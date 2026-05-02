dda337da343be087ec636b0e0efd25a84c15fa48
chandukushal8 <chandukushal8@gmail.com>
2026-05-02 19:39:31 +0530

    Update nodegroup.tf

--- terraform-eks-infra/nodegroup.tf
+++ terraform-eks-infra/nodegroup.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "customer-ng"
   node_role_arn   = aws_iam_role.worker_role.arn
-  subnet_ids      = data.aws_subnets.subnets.ids
+  subnet_ids      = data.aws_subnets.private.ids
 
   scaling_config {
     desired_size = 2
