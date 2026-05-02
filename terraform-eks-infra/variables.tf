77c6906bd823ffeb546aaeeee0700f41ce8ef73a
chandukushal8 <chandukushal8@gmail.com>
2026-05-02 19:38:17 +0530

    Update variables.tf

--- terraform-eks-infra/variables.tf
+++ terraform-eks-infra/variables.tf
@@ -2,10 +2,10 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "vpc_id" {
-  default = "vpc-0e8d7a2787ddcd266"
+variable "cluster_name" {
+  default = "customer-eks"
 }
 
-variable "cluster_name" {
-  default = "customer-cluster"
+variable "vpc_id" {
+  default = "vpc-0e8d7a2787ddcd266"
 }
