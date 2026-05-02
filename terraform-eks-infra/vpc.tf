c04023a211b17851fcfbb5205a129188c9e783a8
chandukushal8 <chandukushal8@gmail.com>
2026-05-02 19:50:37 +0530

    Update vpc.tf

--- terraform-eks-infra/vpc.tf
+++ terraform-eks-infra/vpc.tf
@@ -1,10 +1,29 @@
-data "aws_vpc" "existing" {
-  default = true
+resource "aws_vpc" "eks" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "customer-eks-vpc"
+  }
 }
 
-data "aws_subnets" "private" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.existing.id]
+resource "aws_subnet" "private_1a" {
+  vpc_id            = aws_vpc.eks.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "eks-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1b" {
+  vpc_id            = aws_vpc.eks.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "eks-private-1b"
   }
 }
