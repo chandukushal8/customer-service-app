2a2f421917543419cd38c0f37d81a0357063687e
chandukushal8 <chandukushal8@gmail.com>
2026-05-02 18:28:59 +0530

    Update nodegroup.tf

--- terraform-eks-infra/nodegroup.tf
+++ terraform-eks-infra/nodegroup.tf
@@ -2,7 +2,7 @@ resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "customer-ng"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = data.aws_subnets.subnets.ids
+  subnet_ids      = data.aws_subnets.private.ids
 
   scaling_config {
     desired_size = 2
@@ -13,6 +13,9 @@ resource "aws_eks_node_group" "this" {
   instance_types = ["t3.medium"]
 
   depends_on = [
-    aws_eks_cluster.this
+    aws_eks_cluster.this,
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_CNI_Policy
   ]
 }
